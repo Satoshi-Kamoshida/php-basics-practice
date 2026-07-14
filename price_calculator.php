0cdfdbf63a110fefb7718f776e7bb54c47401e14
Satoshi Kamoshida <duck198771@gmail.com>
2026-07-14 20:58:57 +0900

    fix_unit

--- price_calculator.php
+++ price_calculator.php
@@ -8,8 +8,8 @@ $tax_amount = $subtotal * $tax_rate;
 $total = $subtotal + $tax_amount;
 ?>
 <?php echo '商品名: ' . $product_name; ?><br>
-<?php echo '単価: ' . $price; ?><br>
-<?php echo '数量: ' . $quantity; ?><br>
-<?php echo '小計: ' . $subtotal; ?><br>
-<?php echo '消費税(10%): ' . $tax_amount; ?><br>
-<?php echo '合計金額: ' . $total; ?>
+<?php echo '単価: ' . $price . '円'; ?><br>
+<?php echo '数量: ' . $quantity . '個'; ?><br>
+<?php echo '小計: ' . $subtotal . '円'; ?><br>
+<?php echo '消費税(10%): ' . $tax_amount . '円'; ?><br>
+<?php echo '合計金額: ' . $total . '円'; ?>
